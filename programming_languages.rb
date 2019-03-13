4ceec5bd6290e989c9b6796bd861a52264b8bf74
Jeff M <jkdmarks@gmail.com>
2019-03-13 22:56:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,15 @@
 def reformat_languages(languages)
   new_hash = {}
   
+  ruby = {:type => "interpreted"}
+  javascript = {:type => "interpreted"}
+  python = {:type => "interpreted"}
+  java = {:type => "compiled"}
+  clojure = {:type => "compiled"}
+  erlang = {:type => "compiled"}
+  scala = {:type => "compiled"}
+  javascript = {:type => "interpreted"}
+  
   new_hash[:oo] = {}
   new_hash[:functional] = {}
   
