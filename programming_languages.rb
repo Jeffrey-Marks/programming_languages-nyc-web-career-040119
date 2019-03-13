f92ba3d913c28038522d8563a5aa3f774cc1abd2
Jeff M <jkdmarks@gmail.com>
2019-03-13 23:05:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,15 @@
 def reformat_languages(languages)
   new_hash = {}
   
+  languages.each do |type, langs|
+    langs.each do |lang, lang_data|
+      new_hash[lang] = {
+        lang_data.keys => lang_data.values,
+        :style => [type]
+      }
+    end
+  end
+  
   # ruby = {:type => "interpreted"}
   # javascript = {:type => "interpreted"}
   # python = {:type => "interpreted"}
@@ -9,16 +18,17 @@ def reformat_languages(languages)
   # erlang = {:type => "compiled"}
   # scala = {:type => "compiled"}
   
-  new_hash[:oo] = {
-    :ruby => ruby,
-    :javascript => javascript,
-    :python => python,
-    :java => java
-  }
-  new_hash[:functional] = {
-    :clojure => clojure,
-    :erlang => erlang,
-    :scala => scala,
-    :javascript => javascript
-  }
+  # new_hash[:oo] = {
+  #   :ruby => ruby,
+  #   :javascript => javascript,
+  #   :python => python,
+  #   :java => java
+  # }
+  # new_hash[:functional] = {
+  #   :clojure => clojure,
+  #   :erlang => erlang,
+  #   :scala => scala,
+  #   :javascript => javascript
+  # }
+  new_hash
 end
