284db1d0b8add193b264483af53a4b658d0025ec
Jeff M <jkdmarks@gmail.com>
2019-03-13 23:25:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,15 +2,14 @@ def reformat_languages(languages)
   new_hash = {}
   
   languages.each do |type, langs|
-    langs.each do |lang, lang_data|
-      puts lang_data.keys[0]
-      
-      new_hash[lang] = {}
-      new_hash[lang][lang_data.keys[0]] = lang_data.values[0]
-      if !new_hash[lang][:style]
-        new_hash[lang][:style] = [type]
+    langs.each do |lang, lang_data|    
+      if !new_hash[lang]
+        new_hash[lang] = {}
+        new_hash[lang][lang_data.keys[0]] = lang_data.values[0]
+        new_hash[lang][:style] = []
+        new_hash[lang][:style] << type
       else
-        new_hash[lang][:style].push(type)
+        new_hash[lang][:style] << type
       end
     end
   end
